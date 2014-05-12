4ea55999babb2e72c9cf6bb8ab6b697ab0bc140f
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2014-05-12 11:24:31 +0100

    Added blank c++ main file.

---
+++ src/benchpress.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : benchpress.cpp
+// Author      : Ulrik Kofoed Pedersen
+// Version     :
+// Copyright   : MIT. See LICENSE file.
+// Description : Use blosc with different algorithms to compress data from
+//               datasets in an HDF5 file - and benchmark the performance
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include <blosc.h>
+#include <hdf5_hl.h>
+
+
+int main() {
+	cout << "hi there" << endl; // prints hi there
+	return 0;
+}
